a459c20835cffbd398c44ecbbbba5a7e4fbfc620
Jacob Austin <ausjacob@amazon.com>
2019-04-17 17:59:17 -0700

    Finishing vec3 class

--- weekendraytracer/vec3.h
+++ weekendraytracer/vec3.h
@@ -26,10 +26,10 @@ public:
 	inline double operator[](int i) const { return e[i]; }
 	inline double& operator[](int i) { return e[i]; }
 
-	inline vec3& operator+=(const vec3 &v2);
-	inline vec3& operator-=(const vec3 &v2);
-	inline vec3& operator*=(const vec3 &v2);
-	inline vec3& operator/=(const vec3 &v2);
+	inline vec3& operator+=(const vec3 &v);
+	inline vec3& operator-=(const vec3 &v);
+	inline vec3& operator*=(const vec3 &v);
+	inline vec3& operator/=(const vec3 &v);
 	inline vec3& operator*=(const double t);
 	inline vec3& operator/=(const double t);
 
@@ -48,7 +48,7 @@ public:
 	friend inline vec3 operator*(const vec3 &v1, const vec3 &v2);
 	friend inline vec3 operator/(const vec3 &v1, const vec3 &v2);
 	friend inline vec3 operator*(double t, const vec3 &v);
-	friend inline vec3 operator/(vec3 v, float t);
+	friend inline vec3 operator/(vec3 v, double t);
 	friend inline vec3 operator*(const vec3 &v, double t);
 	friend inline double dot(const vec3 &v1, const vec3 &v2);
 	friend inline vec3 cross(const vec3 &v1, const vec3 &v2);
@@ -92,7 +92,7 @@ inline vec3 operator*(double t, const vec3 &v) {
 	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator/(vec3 v, float t) {
+inline vec3 operator/(vec3 v, double t) {
 	return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
 }
 
@@ -106,6 +106,56 @@ inline double dot(const vec3 &v1, const vec3 &v2) {
 
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
 	return vec3(
-		//todo resume here...
+		  (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
+		(-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
+		  (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0])
 	);
+}
+
+inline vec3& vec3::operator+=(const vec3 &v) {
+	e[0] += v.e[0];
+	e[1] += v.e[1];
+	e[2] += v.e[2];
+	return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3 &v) {
+	e[0] -= v.e[0];
+	e[1] -= v.e[1];
+	e[2] -= v.e[2];
+	return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3 &v) {
+	e[0] *= v.e[0];
+	e[1] *= v.e[1];
+	e[2] *= v.e[2];
+	return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3 &v) {
+	e[0] /= v.e[0];
+	e[1] /= v.e[1];
+	e[2] /= v.e[2];
+	return *this;
+}
+
+inline vec3& vec3::operator*=(const double t) {
+	e[0] *= t;
+	e[1] *= t;
+	e[2] *= t;
+	return *this;
+}
+
+inline vec3& vec3::operator/=(const double t) {
+	double k = 1.0/t;
+	
+	e[0] *= k;
+	e[1] *= k;
+	e[2] *= k;
+	return *this;
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
 }
--- weekendraytracer/weekendraytracer.cpp
+++ weekendraytracer/weekendraytracer.cpp
@@ -1,29 +1,28 @@
 #include <fstream>
 #include <iostream>
+#include "vec3.h"
 
 int main()
 {
 	// open ppm file
-	std::ofstream fs("generated.ppm");
+	std::ofstream fs("generated-vec3-version1.ppm");
 	if (!fs)
 	{
 		std::cerr << "Cannot open the output file." << std::endl;
 		return 1;
 	}
 	
-	int nx = 20;
-	int ny = 10;
+	int nx = 200;
+	int ny = 100;
 
 	fs << "P3\n" << nx << " " << ny << "\n255\n";
 
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			int ir = int(255.99 * col.r());
+			int ig = int(255.99 * col.g());
+			int ib = int(255.99 * col.b());
 
 			fs << ir << " " << ig << " " << ib << "\n";
 		}
